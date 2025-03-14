9ce3e2e810a537ca13c9690a1f128e95b8f3d5d5
David Ikenna Ezekiel <67495047+david-ikenna-ezekiel@users.noreply.github.com>
2025-03-14 22:52:28 +0000

    Updated moving texts

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
   var typed = new Typed('#typed', {
     strings: [
-      "a Software Developer.",
-      "a Creator of Web Experiences.",
-      "passionate about design."
+      "am a Data Engineer.",
+      "love building scalable data infrastructures.",
+      "am passionate about data stories."
     ],
     typeSpeed: 50,
     backSpeed: 30,
