3069911e50461ff9a1ff3de0b6bdf84cf97bb7cf
David Ikenna Ezekiel <67495047+david-ikenna-ezekiel@users.noreply.github.com>
2025-03-17 07:51:39 +0000

    modified layout for mobile view

--- assets/js/script.js
+++ assets/js/script.js
@@ -50,3 +50,13 @@ document.addEventListener('DOMContentLoaded', function() {
     }
   }
 });
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const hamburger = document.querySelector('.hamburger');
+  const nav = document.querySelector('.header-nav');
+
+  hamburger.addEventListener('click', () => {
+    nav.style.display = (nav.style.display === 'block') ? 'none' : 'block';
+  });
+});
